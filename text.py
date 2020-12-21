4361a4c1b1535295cd5a20d08a76b2776cf0acfc
ckrichardson <clifford_richardson@nevada.unr.edu>
2020-12-21 12:30:47 -0800

    Make welcome message prettier

--- text.py
+++ text.py
@@ -1,4 +1,4 @@
-rules = "Hey guys, welcome to r/unr’s discord! Here’s a few rules to get you started.\n\n1:   Be Civil.\n2:   Use the proper channels, and switch over to another channel if the conversation changes to fit that one better.\n3:   Don’t be a narc. Unless someone is hurting someone else (or intending to), it is not ok to ruin someone’s life.\n4:   DO NOT DOXX. Whatever personal information you want to share is fine. If that’s your name, dorm, face, reddit username: ok. But don’t expose other people’s information."
+rules = "Hi, welcome to r/unr’s discord! Here’s a few rules to get you started.\n\n**•   Be Civil.**\n•   **Use the proper channels**, and switch over to another channel if the conversation changes to fit that one better.\n•   **Don’t be a narc**. Unless someone is hurting someone else (or intending to), it is not ok to ruin someone’s life.\n•   **DO NOT DOXX. **Whatever personal information you want to share is fine. If that’s your name, dorm, face, reddit username: ok. But don’t expose other people’s information."
 
 hangman_0 = """```          ________  
 	      |/      | 
