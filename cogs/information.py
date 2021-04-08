7fd0a6da389c594de46a8b9eb843e26f6453c87c
ckrichardson <clifford_richardson@nevada.unr.edu>
2021-04-08 12:30:18 -0700

    Wikipedia command ported to information cog

--- cogs/information.py
+++ cogs/information.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 import helpers
 import os
 import pyowm
+import wikipedia
 
 
 class InformationCog(commands.Cog):
@@ -81,6 +82,91 @@ class InformationCog(commands.Cog):
         embed = discord.Embed(color=consts.color)
         embed.title = "AQI Reno Area"
         embed.description = await helpers.get_aqi()
+        await ctx.send(embed=embed)
+
+
+    # Search wikipedia for something and return the first 3 sentences, and maybe an image
+    @commands.guild_only()
+    @commands.command(pass_context=True)
+    async def wiki(self, ctx, *search):
+        query = ' '.join(search)
+        global color
+
+        if query=="":
+            embed = discord.Embed()
+            embed.description="Usage:   "+prefix+"wiki put_query_here"
+            embed.title = "Wiki"
+            await ctx.send(embed=embed)
+            return 
+
+        try:
+            resolved_page = wikipedia.page(query)
+            url = resolved_page.url
+            summary = wikipedia.summary(query, sentences=3)
+
+            preferred_image_url = None
+            title = resolved_page.title
+            refined_title = title.replace(" ", "_")
+
+            for image in resolved_page.images:
+                if refined_title.lower() in image.lower() and ("jpg" in image or "png" in image):
+                    preferred_image_url = image
+
+            if not preferred_image_url:
+                try:
+                    if len(resolved_page.images):
+                        preferred_image_url = resolved_page.images[2]
+                    else:
+                        None
+                except:
+                    None
+
+            if len(url + "\n\n" + summary) > 2048:
+                statement = (url + "\n\n" + summary)[2045:] + "..."
+            else:
+                statement = url + "\n\n" + summary
+            embed_a = discord.Embed(title=title,description=statement)
+            if preferred_image_url and preferred_image_url:
+                embed_a = embed_a.set_thumbnail(url=preferred_image_url)
+
+        except wikipedia.exceptions.DisambiguationError as e:
+            list_of_options = "Did you mean one of these...?\n\n"
+            counter = 1
+            limit = 20
+
+            if len(e.options) < 20:
+                limit = len(e.options)
+
+            for i in range(limit):
+                list_of_options = list_of_options + str(counter) + ".   " + e.options[i] + "\n"
+                counter += 1
+
+            embed = discord.Embed(title="Oops!", description=list_of_options, color=color)
+
+        except wikipedia.exceptions.PageError as e:
+            embed_a = discord.Embed(title="Oops!", description="Couldn't find what you were looking for!", color=color)
+
+        await ctx.send(embed=embed_a)
+
+
+    # Get covid stats from the coronavirus dashboard of UNR
+    @commands.guild_only()
+    @commands.command(pass_context=True)
+    async def covid(self, ctx): 
+        embed = discord.Embed(color=consts.color)
+        covid_avatar = "https://timesofindia.indiatimes.com/thumb/msid-73928661,width-1200,height-900,resizemode-4/.jpg"
+        embed.set_thumbnail(url=covid_avatar)
+        embed.title = "UNR Covid Cases"
+        data = await helpers.get_covid_data()
+        active = "`{0}`".format(data[0])
+        recovered = "`{0}`".format(data[1])
+        total = "`{0}`".format(data[2])
+        update = "{0}".format(data[3])
+
+        embed.add_field(name="Active", value=active)
+        embed.add_field(name="Recovered", value=recovered, inline=True)
+        embed.add_field(name="Total", value=total, inline=True)
+        embed.set_footer(text=update)
         await ctx.send(embed=embed)
 
 
