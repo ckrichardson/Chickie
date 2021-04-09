100d217c97a691296943c6276de4f1c6ede34841
ckrichardson <clifford_richardson@nevada.unr.edu>
2021-04-09 08:06:09 -0700

    remove unnecessary code, dependencies from main

--- Bot.py
+++ Bot.py
@@ -1,25 +1,5 @@
-import asyncio
-from bs4 import BeautifulSoup
-from copy import deepcopy
-import consts
-import datetime
-from datetime import datetime
-from datetime import timedelta
 import discord as discord
 from discord.ext import commands
-import hashlib
-import helpers
-import json
-import os
-import pyowm
-import pytz
-import random
-import requests
-import sys
-import smtplib
-import text
-import time
-import wikipedia
 
 
 #Intents (this is necessary for the welcome message)
@@ -30,33 +10,13 @@ prefix = '>'
 bot = commands.Bot(command_prefix=prefix, intents=intents, description=":3")
 
 # Cogs
-extensions = ['cogs.pictures',
+extensions = ['cogs.main',
               'cogs.moderation',
               'cogs.information',
               'cogs.utils',
+              'cogs.pictures',
               'cogs.games',
-              'cogs.etc',
-              'cogs.main']
-
-embed = None
-
-# Declare quotes, blacklist
-quotes = ""
-blacklist = list()
-
-
-async def init_vars():
-	# Bot characteristics
-    global embed
-    embed = discord.Embed(color=consts.color)
-    embed.set_thumbnail(url=consts.avatar)
-
-    # Quotes and blacklist
-    global quotes
-    global blacklist
-
-    quotes = await helpers.get_quotes()
-    blacklist = await helpers.get_blacklist()
+              'cogs.etc']
 
 
 for extension in extensions:
